806b0721c101715d85cccf4858c2c33702b77805
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-21 15:14:33 +0000

    everthing

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function myFunction () {
   let age = ''
   age = document.getElementById('my-text').value
   age = parseInt(age)
-  day = document.getElementById('my-text2').value 
+  day = document.getElementById('my-text2').value
   if ((day === 'saturday') && (age > 5 && age < 19)) {
     alert('You can come in the museam with student pricing!')
     // it is one of the two results. it will occur when the user puts down an age larger than 14 and put down a day that is monday or wednesday.
