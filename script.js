5d35b756291a0fa6816ed028ccfef14dc57e8089
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-21 15:03:02 +0000

    changed a lot

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 document.getElementById('my-btn').addEventListener('click', myFunction)
 function myFunction () {
-  let age = document.getElementById('my-text').value
+  let day = ''
+  let age = ''
+  age = document.getElementById('my-text').value
   age = parseInt(age)
-  let day = document.getElementById('my-text2').value
-  day = parseInt(day)
-  if ((day === 'monday' || day === 'wednesday') || (age > 14 && age < 30)) {
-    alert('You can come in the museam!')
+  day = document.getElementById('my-text2').value 
+  if ((day === 'saturday') && (age > 5 && age < 19)) {
+    alert('You can come in the museam with student pricing!')
+    // it is one of the two results. it will occur when the user puts down an age larger than 14 and put down a day that is monday or wednesday.
   } else {
-    alert('You cannot come in the museam.')
+    alert('You cannot come in the museam with student pricing.')
   }
   // it is one of the two results. it will occur when the user puts down an age smaller than 14 or put down a day that isn't monday or wednesday.
 }
